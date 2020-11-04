91df6ce76cc49c0d5b1b6115e231e391deb87c28
volodymyr <vladimir.kondratenko@idapgroup.com>
2020-11-04 16:13:53 +0200

    Lesson 2- New Story, Image, Column, Modifiers

--- app/src/main/java/com/example/trycompose/ui/widgets/newstory/NewStory.kt
+++ app/src/main/java/com/example/trycompose/ui/widgets/newstory/NewStory.kt
@@ -3,10 +3,15 @@ package com.example.trycompose.ui.widgets.newstory
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 import com.example.trycompose.R
@@ -14,17 +19,28 @@ import com.example.trycompose.R
 @Composable
 fun NewStory() {
     val image = imageResource(id = R.drawable.header)
-    
-    Column(modifier = Modifier.padding(16.dp)) {
-        val imageModifier = Modifier
-            .preferredHeight(180.dp)
-            .fillMaxWidth()
 
-        Image(image, modifier = imageModifier, contentScale = ContentScale.Crop)
-        Spacer(modifier = Modifier.preferredHeight(16.dp))
-        Text("A day in Shark Fin Cove")
-        Text("Davenport, California")
-        Text("December 2018")
+    MaterialTheme() {
+
+        Column(modifier = Modifier.padding(16.dp)) {
+            val imageModifier = Modifier
+                .preferredHeight(180.dp)
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(4.dp))
+
+            Image(image, modifier = imageModifier, contentScale = ContentScale.Crop)
+            Spacer(modifier = Modifier.preferredHeight(16.dp))
+            Text(
+                "A day wandering through the sandhills " +
+                        "in Shark Fin Cove, and a few of the " +
+                        "sights I saw",
+                style = typography.h6,
+                maxLines = 2,
+                overflow =TextOverflow.Ellipsis
+            )
+            Text("Davenport, California", style = typography.body2)
+            Text("December 2018", style = typography.body2)
+        }
     }
 }
 
