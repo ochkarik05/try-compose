6863a555230acc25d6a7b94697d08c5e0ad3e4bc
volodymyr <vladimir.kondratenko@idapgroup.com>
2020-11-17 13:20:59 +0200

    Update Libs

--- app/src/main/java/com/example/trycompose/MainActivity.kt
+++ app/src/main/java/com/example/trycompose/MainActivity.kt
@@ -3,9 +3,7 @@ package com.example.trycompose
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.ComposeView
-import androidx.ui.tooling.preview.Preview
 import com.example.trycompose.ui.widgets.main.Main
 import com.example.trycompose.ui.widgets.nav.NestedNavCompose
 
